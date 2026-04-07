e35f787640f11d820c7061a66bc732adfe8e77b5
sumsur29 <84914740+sumsur29@users.noreply.github.com>
2026-04-07 12:57:27 +0400

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import ProteinTracker from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ProteinTracker />
-  </React.StrictMode>
-)
